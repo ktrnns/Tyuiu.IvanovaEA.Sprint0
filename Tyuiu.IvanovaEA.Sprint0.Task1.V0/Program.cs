41ce71a150b43d355a890372e4a2824d5bdfeddb
“Екатерина <kataivanova0380@gmail.com>
2026-09-08 20:57:42 +0500

    Add Task1

---
+++ Tyuiu.IvanovaEA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IvanovaEA.Sprint0.Task1.V0
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string name = "Екатерина";
+
+            int age = 20;
+
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + ", мне " + age + " лет.");
+
+            Console.ReadKey();
+        }
+    }
+}           
