1eed9791f7dd41c57846f95f816dbef50bfa8280
“Екатерина <kataivanova0380@gmail.com>
2026-09-09 20:48:10 +0500

    Add Task6

---
+++ Tyuiu.IvanovaEA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.IvanovaEA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        // с параметром
+        public static object AdditionArray(int[] numbers)
+        {
+            var total = 0;
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                total = total + numbers[i];
+            }
+            return total;
+        }
+
+        // с предусловием
+        public static object SubstractionArray(int[] numbers)
+        {
+            var total = 0;
+            int index = 0;
+
+            while (index < numbers.Length) 
+            {
+                total = total - numbers[index];
+                index++;
+            }
+            return total;
+        }
+
+        // с постусловием
+        public static object MultiplicationArray(int[] numbers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * numbers[index];
+                index++;
+            }
+            while (index < numbers.Length);
+
+            return total;   
+        }
+    }
+}
